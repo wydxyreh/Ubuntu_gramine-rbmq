b0d11dafe90bb462b49eb1e3c5335dddf3e01d62
wydxyreh <18180006982@163.com>
2022-11-22 20:39:24 +0800

    Add files via upload

---
+++ code/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#import subprocess
+#subprocess.call(["ping", "www.baidu.com"])
+
+
+import subprocess
+#completed = subprocess.run(['rabbitmq-server'])
+completed = subprocess.run(['sudo','rabbitmq-server'])
+#completed = subprocess.run(['python'])
+#completed = subprocess.run([''])
+#completed = subprocess.run(['/usr/local/rabbitmq/./rabbitmq-server', '-1'])
+#completed = subprocess.run(['ls', '-1'])
+
+#print('returncode:', completed.returncode)
+
+
+
